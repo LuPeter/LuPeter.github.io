64c9884612362bb306068934a8a7ba61e55ae65e
LuPeter <p23609857@hotmail.com>
2022-08-28 04:06:24 +0800

    chore: show last update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,6 +37,7 @@ const config = {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl: 'https://github.com/LuPeter/LuPeter.github.io/tree/master',
+					showLastUpdateTime: true,
 				},
 				blog: {
 					showReadingTime: true,
